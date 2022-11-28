b828b16db72167868aa06da61761486a93937dc9
Girija Patil <girija.patil@gyansys.com>
2022-11-28 13:07:04 +0530

    first commit

---
+++ PortfolioMVCproject/App_Start/RouteConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PortfolioMVCproject
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "Education",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Education", action = "Education", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+               name: "Experience",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Experience", action = "Experience", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+               name: "Skills",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Skills", action = "Skills", id = UrlParameter.Optional }
+           );
+            routes.MapRoute(
+               name: "Hobbies",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Hobbies", action = "Hobbies", id = UrlParameter.Optional }
+           );
+        }
+    }
+}
